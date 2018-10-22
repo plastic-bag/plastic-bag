d3035cc38be572ecc51214fd595ca1404da183ed
Roberto Soares <roberto.tech@gmail.com>
2018-10-22 14:58:17 -0300

    Add test for dynamic scenarios. co-author: @gbmoretti

--- test/browsers/examples.js
+++ test/browsers/examples.js
@@ -1,48 +1,54 @@
 var assert = require('assert');
 
-describe('Examples', () => {
-  before(() => {
-    browser.url('/test/html/index.html');
-  });
-  const itBehavesLikeBasicExample = ({ setUp, example }) => {
-    const button = () => $(example).element('button');
+const itBehavesLikeExample = url => ({ setUp, example }) => {
+  const button = () => $(example).element('button');
 
-    describe(`example ${example}`, () => {
-      before(() => {
-        setUp && setUp();
+  describe(example, () => {
+    before(() => {
+      browser.url(url);
+      setUp && setUp();
+    });
+
+    context('on start', () => {
+      it(`shows only an example of ${example}`, () => {
+        assert.equal(browser.elements(example).value.length, 1);
       });
 
-      context('on start', () => {
-        it('has a button with default state', () => {
-          const classList = button().getAttribute('class');
-          assert.equal(classList, 'button is-small');
-        });
-      });
-
-      context('on click', () => {
-        beforeEach(() => {
-          button().click();
-        });
-
-        it('changes button to success', () => {
-          const classList = button().getAttribute('class');
-          assert.equal(classList, 'button is-small is-success');
-        });
+      it('has a button with default state', () => {
+        const classList = button().getAttribute('class');
+        assert.equal(classList, 'button is-small');
       });
     });
-  };
 
-  itBehavesLikeBasicExample({
-    example: '#html-fragment-example',
+    context('on click', () => {
+      beforeEach(() => {
+        button().click();
+      });
+
+      it('changes button to success', () => {
+        const classList = button().getAttribute('class');
+        assert.equal(classList, 'button is-small is-success');
+      });
+    });
   });
-  itBehavesLikeBasicExample({
-    example: '#script-example',
-  });
-  itBehavesLikeBasicExample({
-    example: '#iframe-example',
-    setUp: () => {
-      const frame = $('iframe').value;
-      browser.frame(frame);
-    },
+};
+
+['/test/html/index.html', '/test/html/dynamic.html'].forEach(url => {
+  describe('Examples -', () => {
+    const itBehavesLikeBasicExample = itBehavesLikeExample(url);
+
+    itBehavesLikeBasicExample({
+      example: '#html-fragment-example',
+    });
+    itBehavesLikeBasicExample({
+      example: '#script-example',
+    });
+    itBehavesLikeBasicExample({
+      example: '#iframe-example',
+      setUp: () => {
+        const frame = $('iframe').value;
+        browser.frame(frame);
+      },
+    });
   });
 });
